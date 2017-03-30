4f4547f03f2ab3e43c153e987e4fbef5c521f961
flo <artemjjke@gmail.com>
2017-03-31 01:23:07 +0200

    added dummy web ui project

---
+++ src/Rajah.WebUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Rajah.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5101")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ src/Rajah.WebUI/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿var HttpClient = function () {
+    this.get = function (url, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200)
+                callback(httpRequest.responseText);
+        }
+
+        httpRequest.open("GET", url, true);
+        httpRequest.setRequestHeader("Access-Control-Allow-Origin", "*");
+        httpRequest.send(null);
+    }
+}
+
+window.onload = function () {
+    function UpdateStatus() {
+        var client = new HttpClient();
+        client.get('http://192.168.1.165:5100/api/device', function (response) {
+            document.getElementById("status").innerText = response;
+        });
+    }
+
+    setInterval(UpdateStatus, 300);
+}
